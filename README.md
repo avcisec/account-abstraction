3b6b9445943eee8e5473e172c65521200c89643d
avcisec <yasaravci@protonmail.com>
2025-04-05 17:28:47 +0200

    lets gooooo

---
+++ README.md
@@ -0,0 +1,7 @@
+## About
+
+1. Create a basic AA on ethereum
+2. Create a basic AA on zksync
+3. Deploy, and send a userOp/tx through them
+   1. Not going to send an AA to EVM
+   2. But will send an AA tx to ZKevm
---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {IAccount} from "@account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+contract MinimalAccount is IAccount {
+    // entrypoint kontratı bu kontratı çağıracak.
+
+        function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData) {}
+
+}
